2418e3e323bb84ab2fd89b1c870d32c14dbe2f19
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-04-25 16:54:25 +0800

    Added some details

--- portfolio/pages/urls.py
+++ portfolio/pages/urls.py
@@ -1,8 +1,12 @@
-# pages/urls.py
+# urls.py within the pages app
 
 from django.urls import path
-from pages import views
+from . import views
 
 urlpatterns = [
-    path("", views.home, name='home'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('portfolio/', views.portfolio, name='portfolio'),  # Add this line for portfolio
+    path('contact/', views.contact, name='contact'),
+    # Other URL patterns
 ]
--- portfolio/pages/views.py
+++ portfolio/pages/views.py
@@ -1,5 +1,16 @@
+# views.py within the pages app
+
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
     return render(request, "pages/home.html", {})
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def portfolio(request):
+    # You can add any necessary context data here
+    return render(request, 'projects/project_index.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
